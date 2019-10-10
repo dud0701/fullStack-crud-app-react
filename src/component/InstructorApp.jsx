b8135e9eff349c068637b2078610407848dec018
cye <cye@vivans.net>
2019-10-10 18:47:11 +0900

    route

--- src/component/InstructorApp.jsx
+++ src/component/InstructorApp.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import ListCoursesComponent from './ListCoursesComponent';
+import { Router, Switch, Route } from 'react-router-dom';
+import CourseComponent from './CourseComponent';
 
 class InstructorApp extends Component {
     render(){
-        return(<>
+        return(
+        <Router>
+        <>
             <h1>Instructor Application</h1>
-            <ListCoursesComponent />
+            <Switch>
+                <Route exact path="/" component={ListCoursesComponent} />
+                <Route exact path="/courses" component={ListCoursesComponent}/>
+                <Route path="/courses/:id" component={CourseComponent} />
+            </Switch>
+            
             </>
+            </Router>
         )
     }
 }
